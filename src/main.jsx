ed70c960967a18bf97cd82c7a1035ff9ec54fc72
AhmadSaadAlden <ahmadsaad01045@gmail.com>
2025-06-24 21:07:33 +0300

    add path

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SignIn from './Pages/Auth/SignIn.jsx'
 import SignUp from './Pages/Auth/SignUp.jsx'
-import Dashboard from './Pages/dashboard/dashboard.jsx'
 import AddProductForm from './assets/Components/AllProduct/AddProductForm/AddProductForm.jsx'
 import EditProductForm from './assets/Components/AllProduct/EditProductForm/EditProductForm.jsx'
 import DeletProductForm from './assets/Components/AllProduct/DeletProductForm/DeletProductForm.jsx'
 import DeleteProductPage from './assets/Components/AllProduct/DeletProductForm/DeleteProductPage.jsx'
 import { ProductProvider } from './assets/Components/AllProduct/ProductContext/ProductContext.jsx'
 import { NotificationProvider } from './assets/Components/AllProduct/ProductContext/NotificationContext.jsx'
+import Dashboard from './Pages/Dashboard/Dashboard.jsx'
 
 const router = createBrowserRouter([
   {
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
   },
   {
     path : "dashboard",
-    element : <Dashboard />,
+    element : <Dashboard/>,
     children: [
       {
         path: "add-product",
